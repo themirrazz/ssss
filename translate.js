e1a43fb182a802f5dcdef36c0853ee4c9b6fc19d
themirrazz <89490659+themirrazz@users.noreply.github.com>
2024-01-10 10:30:12 -0800

    Update translate.js

--- translate.js
+++ translate.js
@@ -1,8 +1,29 @@
 (function () {
     var links = document.querySelectorAll('.localize-link');
-    var uu = new URL(location.href);
-    uu.hostname = "jp.themirrazz.repl.co";
-    links[1].href = uu.href;
+    var languages = [
+        'en',
+        'es',
+        'fr',
+        'de',
+        'zh',
+        'ja',
+        'ar',
+        'tlh'
+    ];
+    links.forEach((link,index3) => {
+        var uu = new URL(location.href);
+        var d = document.querySelector('html').getAttribute('lang');
+        uu.pathname = uu.pathname.replace(
+            '/'+d+'/',
+            '/'+languages[index3]+'/'
+        );
+        if(d!=languages[index3]) {
+            link.href=uu.href;
+            link.className = 'localize-link';
+        } else {
+            link.className = 'localize-link localize-current';
+        }
+    });
     document.querySelector('.header-translate').onclick = function (event) {
         if(event.stopPropagation) {
             event.stopPropagation();
